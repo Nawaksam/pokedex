95f7a4238e8f22dc12601bcbe062fed45592982f
Nawaksam <sam.cardonnel@gmail.com>
2023-05-31 13:46:19 +0200

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,25 @@ import { useState } from "react"
 import "./App.css"
 import PokemonCard from "./components/PokemonCard.jsx"
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+]
+
 function App() {
   return (
     <>
       <h1>Attrapez-les tous !</h1>
       <p>...</p>
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonList[0]} />
     </>
   )
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,31 +1,13 @@
 import "./PokemonCard.css"
 
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-]
-
-const isSrc = (item) => {
-  if (item.imgSrc) {
-    return <img className="pokemon-avatar" src={item.imgSrc} alt={item.name} />
-  } else {
-    return <p>???</p>
-  }
-}
-
-const PokemonCard = () => {
-  const pokemon = pokemonList[0]
+// prettier-ignore
+const PokemonCard = (props) => {
+  const pokemon = props.pokemon
   return (
     <>
       <div className="pokemon-card">
         <figure>
-          {isSrc(pokemon)}
+          {pokemon.imgSrc? <img className="pokemon-avatar" src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
           <figcaption>{pokemon.name}</figcaption>
         </figure>
       </div>
